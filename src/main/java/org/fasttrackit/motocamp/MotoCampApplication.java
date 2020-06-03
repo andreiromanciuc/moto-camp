c9f8367db1c3746dc6d03969035d455bf9ac0f9a
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-06-03 11:12:48 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/motocamp/MotoCampApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.motocamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/motocamp/MotoCampApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.motocamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
